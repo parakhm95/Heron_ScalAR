acbfb2ffc44e82dd9a5ee22627831daee983974d
Parakh <parakhm95@gmail.com>
2019-05-03 21:55:50 -0400

    Heron ROS Package

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Heron-position-controller
 Python file for controlling position of Heron using GPS and ROS topics.
+
+cmd_drive = command for thrusters
+cmd_helm = Thrust, Yaw rate
+Not a perfect circle but tries
+cmd_wrench = Force vector, Torque Vector
+Reliable and rotates in the correct position
+cmd_course = yaw, speed = yaw doesn't work. Speed seems to work.
+
+
+/imu/compass_heading = data = nan
+/imu/declination = Nothing
+/imu/raw_compass_heading = West is 0, North is 1.57, East is 3.14/-3.14, South is -1.57
+/imu/data = Quaternion Orientation, Angular velocities, Linear acceleration  
+/imu/mag = magnetometer data in x,y,z              
+/imu/rpy = vector x,y,z
+/imu/data_compass =  angular velocity x,y,z, linear acceleration x,y,z         
+/imu/mag_calib            
+/imu/temperature
+
+In joystick, both the thrusters don't work together.
--- scripts/data_extraction.py
+++ scripts/data_extraction.py
@@ -9,12 +9,12 @@ latitude = []
 longitude = []
 file = open('data11.csv','a')
 data = csv.writer(file, delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
-ser = serial.Serial()
-ser.baudrate = 9600
-ser.port = '/dev/ttyUSB0'
-ser.timeout = 0.1
-ser.open()
-ser.reset_input_buffer()
+# ser = serial.Serial()
+# ser.baudrate = 9600
+# ser.port = '/dev/ttyUSB0'
+# ser.timeout = 0.1
+# ser.open()
+# ser.reset_input_buffer()
 micron = "sounder"
 
 
@@ -33,15 +33,15 @@ def spit():
     print longitude
     file.close()
 
-def serdataget():
-    global micron
-        if(ser.in_waiting):
-        lmao = ser.readline()
-        print(lmao.find(',M,'))
-        if(lmao.find(',M,') != -1):
-            micron = lmao[16]+lmao[17]+lmao[18]+lmao[19]+lmao[20]+lmao[21]
-        # print(lmao[16]+lmao[17]+lmao[18]+lmao[19]+lmao[20]+lmao[21])
-    return
+# def serdataget():
+#     global micron
+#     if(ser.in_waiting):
+#         lmao = ser.readline()
+#         print(lmao.find(',M,'))
+#         if(lmao.find(',M,') != -1):
+#             micron = lmao[16]+lmao[17]+lmao[18]+lmao[19]+lmao[20]+lmao[21]
+#         # print(lmao[16]+lmao[17]+lmao[18]+lmao[19]+lmao[20]+lmao[21])
+#     return
 
 
 
@@ -53,7 +53,7 @@ def data_extraction():
     freq = 100  # hz
     rate = rospy.Rate(freq)
     while not rospy.is_shutdown():
-        serdataget()
+        # serdataget()
         rate.sleep()
         # print("running")
 
