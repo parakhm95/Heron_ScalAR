de872c395710887f8b73c9d1748337c45eb79b24
Dhanushka Kularatne <dhanushkakularatne@gmail.com>
2019-06-14 16:33:32 -0400

    add waypoint navigator to publish helm commands

--- scripts/waypoint_navigator.py
+++ scripts/waypoint_navigator.py
@@ -20,9 +20,9 @@ course_desired = ([49.9000000007,8.89999999997],
 	[49.9000000007,8.89999999997])
 i = 0
 kp=1
-wypt_dist_thresh = 2.0
-cmd_dt = 0.1
-base_thrust = 1.0
+wypt_dist_thresh = 2.0  # distance threshold to stop or switch to next way point
+cmd_dt = 0.1            # time interval for cmd_helm publishing
+base_thrust = 1.0       # the base thrust level at which the Heron will run
 
 def nav_comp(navsat_msg):
     global pos_cur
@@ -37,7 +37,7 @@ def yaw_callback(Float32_msg):
     return
 
 
-def control_pulisher(event):
+def control_publisher(event):
     global yaw_cur, i, kp, pos_cur, yaw_des_old, course_desiredi, wypt_dist_thresh, cmd_dt, base_thrust
     pub_msg = Helm()
     helm_pub = rospy('/cmd_helm', Helm, queue_size=100)
@@ -46,12 +46,15 @@ def control_pulisher(event):
     pos_des_lon = course_desired[i][1]
 
     dist_err = math.sqrt( (pos_des_lat-pos_cur.latitude)**2 + (pos_des_lon - pos_cur.longitude)**2 )
+    
     if( dist_err < wypt_dist_thresh) 
+        # go to next way point if within distance threshold
         if( i < len(course_desired)-1 ):
             i = i+1
             pos_des_lat = course_desired[i][0]
             pos_des_lon = course_desired[i][1]
             dist_err = math.sqrt( (pos_des_lat-pos_cur.latitude)**2 + (pos_des_lon - pos_cur.longitude)**2 )
+        # stop if at last waypoint
         else:
             pub_msg.thrust = 0.0
             pub_msg.yaw_rate = 0.0
@@ -62,38 +65,44 @@ def control_pulisher(event):
     if( yaw_des < 0):
         yaw_des = 2*math.pi + yaw_des
 
+    # compute rate of change of yaw_des
     yaw_des_rate = (yaw_des-yaw_des_old)/cmd_dt;
     yaw_des_old = yaw_des;
 
+    # compute yaw_error and change it to be within [-pi pi]
     yaw_error = yaw_des - yaw_cur
     if ( yaw_error > math.pi ):
         yaw_error = yaw_error-2*math.pi
     elif ( yaw_error < -math.pi ):
         yaw_error = 2*math.pi + yaw_error
 
+    # controller for yaw_rate
     pub_msg.yaw_rate = kp*yaw_error + yaw_des_rate
+    
+    # if far away, thrusht is at base thrust level
     if( dist_error > 10 ):
         pub_msg.thrust = base_thrust
+    # gradually slow down as wel approach the waypoint
     else:
         pub_msg.thrust = base_thrust*dist_error/10.0
 
+    # adjust thrust according to yaw_error, i.e., adjust yaw first before thrusting forwayd
     pub_msg.thrust = pub_msg.thrust*math.exp(-10*math.fabs(yaw_error) )
+    
+    # publish message on topic
     helm_pub.publish(pub_msg)
     return
     
 def course_publisher():
     global cmd_dt
 
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # node are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
     rospy.init_node('wapt_control_publisher', anonymous=True)
     rospy.Subscriber("/navsat/fix", NavSatFix,nav_comp)
     
     # /filtered_yaw is the topic on which the data from the complementary filter will arrive
     rospy.Subscriber("/filtered_yaw", Float32, yaw_callback)
+    
+    # publishes data on cmd_helm every cmd_dt seconds
     rospy.Timer(rospy.Duration(cmd_dt), control_publisher)
 
     # spin() simply keeps python from exiting until this node is stopped
